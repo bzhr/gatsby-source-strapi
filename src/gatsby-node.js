b4c16e4bd3d7da8b1b7a2fafb9c4e8e650a4bc3a
Bozhdidar <bozidar.hristov@gmail.com>
2018-05-25 14:14:44 +0200

    Functional child nodes for images

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,25 +1,25 @@
-import axios from "axios"
-import fetchData from "./fetch"
-import { Node } from "./nodes"
-import { capitalize } from "lodash"
+import axios from 'axios'
+import fetchData from './fetch'
+import { Node } from './nodes'
+import { capitalize } from 'lodash'
 const crypto = require(`crypto`)
 
 exports.sourceNodes = async (
   { boundActionCreators },
-  { apiURL = "http://localhost:1337", contentTypes = [], loginData = {} }
+  { apiURL = 'http://localhost:1337', contentTypes = [], loginData = {} }
 ) => {
-  const { createNode } = boundActionCreators
+  const { createNode, createParentChildLink } = boundActionCreators
   let jwtToken = null
 
   // Check if loginData is set.
   if (
-    loginData.hasOwnProperty("identifier") &&
+    loginData.hasOwnProperty('identifier') &&
     loginData.identifier.length !== 0 &&
-    loginData.hasOwnProperty("password") &&
+    loginData.hasOwnProperty('password') &&
     loginData.password.length !== 0
   ) {
-    console.time("Authenticate Strapi user")
-    console.log("Authenticate Strapi user")
+    console.time('Authenticate Strapi user')
+    console.log('Authenticate Strapi user')
 
     // Define API endpoint.
     const loginEndpoint = `${apiURL}/auth/local`
@@ -28,14 +28,14 @@ exports.sourceNodes = async (
     try {
       const loginResponse = await axios.post(loginEndpoint, loginData)
 
-      if (loginResponse.hasOwnProperty("data")) {
+      if (loginResponse.hasOwnProperty('data')) {
         jwtToken = loginResponse.data.jwt
       }
     } catch (e) {
-      console.error("Strapi authentication error: " + e)
+      console.error('Strapi authentication error: ' + e)
     }
 
-    console.timeEnd("Authenticate Strapi user")
+    console.timeEnd('Authenticate Strapi user')
   }
 
   // Generate a list of promises based on the `contentTypes` option.
@@ -49,7 +49,7 @@ exports.sourceNodes = async (
 
   // Execute the promises.
   const data = await Promise.all(promises)
-  
+
   // Create nodes.
   contentTypes.forEach((contentType, i) => {
     const items = data[i]
@@ -57,59 +57,66 @@ exports.sourceNodes = async (
     items.forEach(item => {
       const node = Node(capitalize(contentType), item)
       createNode(node)
-      findChildNodes(node, childNodes, node, contentType)
-      childNodes.forEach(node => createNode(node))
-      // childNodes.forEach(node => console.log(node.id))
+      findChildNodes(node, childNodes, node, createNode, createParentChildLink)
     })
+    // childNodes.forEach(node => createNode(node))
   })
 }
 
-const findChildNodes = (item, childNodes, parent, contentType) => {
+const findChildNodes = (item, childNodes, parent, createNode, createParentChildLink) => {
   const keys = Object.keys(item)
   keys.forEach(key => {
     if (Array.isArray(item[key])) {
-      if (key === "images") {
+      if (key === 'images') {
         item[key].forEach(i => {
-          if ("mime" in i) {
-            console.log(`${contentType}_${i.id}`)
+          if ('mime' in i) {
             const node = {
-              id: `${contentType}_${i.id}`,
+              id: i.id,
               parent: parent.id,
               children: [],
-              image: i["url"],
+              url: i['url'],
+              name: i['name'],
               internal: {
                 type: `ImageField`,
-                mediaType: i["mime"],
-                content: i["url"],
+                mediaType: i['mime'],
+                content: i['url'],
                 contentDigest: crypto
-                  .createHash("md5")
-                  .update(JSON.stringify(i["url"]))
-                  .digest("hex")
-              }
-            };
-            childNodes.push(node)
+                  .createHash('md5')
+                  .update(JSON.stringify(i['url']))
+                  .digest('hex'),
+              },
+            }
+            if (node) {
+              // childNodes.push(node)
+              createNode(node)
+              createParentChildLink({ parent: parent, child: node })
+            }
           }
         })
       }
-    } else if (typeof item[key] === "object") {
-      if ("mime" in item[key]) {
-        console.log(`${contentType}_${item[key].id}`)
+    } else if (typeof item[key] === 'object') {
+      if ('mime' in item[key]) {
         const node = {
-          id: `${contentType}_${item[key].id}`,
+          id: item[key].id,
           parent: parent.id,
           children: [],
-          image: item[key]["url"],
+          url: item[key]['url'],
+          name: item[key]['name'],
           internal: {
             type: `ImageField`,
-            mediaType: item[key]["mime"],
-            content: item[key]["url"],
+            mediaType: item[key]['mime'],
+            content: item[key]['url'],
             contentDigest: crypto
-              .createHash("md5")
-              .update(JSON.stringify(item[key]["url"]))
-              .digest("hex")
-          }
-        };
-        childNodes.push(node)
+              .createHash('md5')
+              .update(JSON.stringify(item[key]['url']))
+              .digest('hex'),
+          },
+        }
+        if (node) {
+          // childNodes.push(node)
+          createNode(node)
+          createParentChildLink({ parent: parent, child: node })
+        }
       }
     }
   })
